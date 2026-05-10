ee144dcfac1f88e6eec9b8eb84a3bc15378c4d33
Brody <brodytmcallister@gmail.com>
2026-05-10 17:40:35 -0400

    added ability to open p3 files and changed command

--- main.c
+++ main.c
@@ -1,42 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+
+// imgpc [input filename] [effect] [output filename] [output encoding]
 
 int main(int argc, char *argv[]) {
     typedef enum effect {
-        INVERT, GREYSCALE, BW, PIXELATE, NONE
+        INVERT, GREYSCALE, BW, PIXELATE, BLUEISH, NONE
     } effect;
 
     effect fx = NONE;
+    char *infilename;
     char *outfilename;
 
     // process args
 
-    if (argc > 3) {
-        printf("Greater than 2 args passed. Exiting...\n");
-        printf("Usage: cat {input} | ./main {effect} {output}\n");
+    if (argc > 5) {
+        printf("Too many arguments. Exiting...\n");
+        printf("Usage: ./main [input filename] [effect] [output filename] [output encoding]\n");
         return 1;
     }
-    else if (argv[1] && argv[2]) {
-        if (strcmp(argv[1], "invert") == 0) { fx = INVERT; }
-        else if (strcmp(argv[1], "greyscale") == 0) { fx = GREYSCALE; }
-        else if (strcmp(argv[1], "blackandwhite") == 0) { fx = BW; }
-        else if (strcmp(argv[1], "pixelate") == 0) { fx = PIXELATE; }
-        else if (strcmp(argv[1], "none") == 0) { fx = NONE; }
-        else if (strcmp(argv[1], "help") == 0) {
-            printf("Usage: cat {input} | ./main {effect} {output}\n");
+    else if (argv[1] && argv[2] && argv[3] && argv[4]) {
+        infilename = (argv[1]); // should check if this is valid
+
+        if (strcmp(argv[2], "invert") == 0) { fx = INVERT; }
+        else if (strcmp(argv[2], "greyscale") == 0) { fx = GREYSCALE; }
+        else if (strcmp(argv[2], "blackandwhite") == 0) { fx = BW; }
+        else if (strcmp(argv[2], "pixelate") == 0) { fx = PIXELATE; }
+        else if (strcmp(argv[2], "blueish") == 0) { fx = BLUEISH; }
+        else if (strcmp(argv[2], "none") == 0) { fx = NONE; }
+        else if (strcmp(argv[2], "help") == 0) {
             return 1;
         }
         else {
-            printf("Unknown fx. Exiting...\n");
+            printf("Unknown fx '%s'. Exiting...\n", argv[2]);
+            printf("Usage: ./main [input filename] [effect] [output filename] [output encoding]\n");
             return 1;
         }
 
-        outfilename = (strcat(argv[2], ".ppm"));
+        outfilename = (argv[3]); // should check if this is valid
+
+        char *output_enc = argv[4];
+
+        if (strcmp(output_enc, "p3") != 0 && strcmp(output_enc, "p6") != 0) {
+            printf("Output encoding much be 'p3' or 'p6'. Is: %s. Exiting...\n", argv[4]);
+            printf("Usage: ./main [input filename] [effect] [output filename] [output encoding]\n");
+            return 1;
+        }
+
     }
     else {
         printf("Too few args passed. Exiting...\n");
-        printf("Usage: cat {input} | ./main {effect} {output}\n");
+        printf("Usage: ./main [input filename] [effect] [output filename] [output encoding]\n");
+        return 1;
+    }
+
+    infilename = strcat(infilename, ".ppm");
+    outfilename = strcat(outfilename, ".ppm");
+
+    FILE *fptr = fopen(infilename, "rb");
+
+    if (fptr == NULL) {
+        printf("File %s does not exist. Exiting...\n", infilename);
         return 1;
     }
 
@@ -45,8 +71,8 @@ int main(int argc, char *argv[]) {
     char magic_number[3];
     int width, height, maxval;
 
-    fscanf(stdin, "%s", magic_number);
-    fscanf(stdin, "%d %d %d", &width, &height, &maxval);
+    fscanf(fptr, "%s", magic_number);
+    fscanf(fptr, "%d %d %d", &width, &height, &maxval);
 
     // check header
 
@@ -55,19 +81,24 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    if (magic_number[1] == '3') { // TODO: process P3 at some point
-        printf("Image must be P6. Exiting...\n");
-        return 1;
-    }
-
     int num_pixels = width * height;
-
     unsigned char *pixels;
 
     pixels = malloc(num_pixels * 3);
     if (!pixels) { perror("malloc"); return 1; }
 
-    fread(pixels, 3, num_pixels, stdin); // read pixels
+    if (magic_number[1] == '3') {
+        for (int i = 0; i < num_pixels * 3; i++) {
+            int val;
+            fscanf(fptr, "%d", &val);
+            pixels[i] = (unsigned char)val;
+        }
+    }
+    else if (magic_number[1] == '6') {
+        fread(pixels, 3, num_pixels, fptr); // read pixels
+    }
+
+    // EFFECTS
 
     unsigned char avg;
 
@@ -115,6 +146,13 @@ int main(int argc, char *argv[]) {
             break;
         case PIXELATE:
             break;
+        case BLUEISH:
+            for (int i = 0; i < num_pixels * 3; i += 3) {
+                pixels[i] = 0;
+                pixels[i+1] = 160;
+                // pixels[i+2] = 0;
+            }
+            break;
         case NONE:
             break;
         default:
@@ -127,7 +165,7 @@ int main(int argc, char *argv[]) {
 
     FILE *outptr = fopen(outfilename, "w");
 
-    int export = 6; // export as P3 or P6?
+    int export = 3; // export as P3 or P6?
 
     if (export == 3) { //export as P3 (plain text)
         fprintf(outptr, "%s\n", "P3");
@@ -138,7 +176,7 @@ int main(int argc, char *argv[]) {
         }
     }
     else if (export == 6) { //export as P6 (binary)
-        fprintf(outptr, "%s\n", magic_number);
+        fprintf(outptr, "%s\n", "P6");
         fprintf(outptr, "%d %d\n%d\n", width, height, maxval);
         fwrite(pixels, 3, num_pixels, outptr);
     }
